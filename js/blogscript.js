8cc3fdb56928c6839fd1558d21a6c0403ffb76d0
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-23 22:39:35 +0530

    add project sub page and blog sub page

---
+++ js/blogscript.js
@@ -0,0 +1,79 @@
+window.onload = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const blog = urlParams.get("blog");
+    const blogDocumentTitle = document.querySelector("title");
+    const blogBannerTitle = document.querySelector(".blog-banner-title");
+    const blogBannerImage = document.querySelector(".blog-banner-img");
+    const blogImage1 = document.querySelector(".blog-img-1");
+    const blogImage2 = document.querySelector(".blog-img-2");
+    const blogAdmin = document.querySelector(".blog-admin");
+    const blogDate = document.querySelector(".blog-date");
+    const blogComments = document.querySelector(".blog-comments");
+    const blogContent1 = document.querySelector(".blog-content-1");
+    const blogContent2 = document.querySelector(".blog-content-2");
+    const blogContent3 = document.querySelector(".blog-content-3");
+    const blogContent4 = document.querySelector(".blog-content-4");
+
+    switch (blog) {
+        case "Saving the Rainforests":
+            updateBlog1Content();
+            break;
+        case "Eco-friendly Travel Tips":
+            updateBlog2Content();
+            break;
+        case "The Role of National Parks":
+            updateBlog3Content();
+            break;
+        default:
+            updateBlog1Content();
+            break;
+    }
+
+    function updateBlog1Content() {
+        blogDocumentTitle.textContent = "Saving the Rainforests";
+        blogBannerTitle.textContent = "Saving the Rainforests";
+        blogBannerImage.src = '/media/giraffe-2.jpg';
+        blogImage1.src = '/media/blogcardimg1.jpg';
+        blogImage2.src = '/media/blogcardimg1.jpg';
+        blogAdmin.textContent = 'admin';
+        blogDate.textContent = '23 July, 2024',
+        blogComments.textContent = '0',
+        blogContent1.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent2.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent3.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent4.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+    
+    }
+
+    function updateBlog2Content() {
+        blogDocumentTitle.textContent = "Eco-friendly Travel Tips";
+        blogBannerTitle.textContent = "Eco-friendly Travel Tips";
+        blogBannerImage.src = '/media/giraffe-2.jpg';
+        blogImage1.src = '/media/blogcardimg2.jpg';
+        blogImage2.src = '/media/blogcardimg2.jpg';
+        blogAdmin.textContent = 'admin';
+        blogDate.textContent = '23 July, 2024',
+        blogComments.textContent = '0',
+        blogContent1.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent2.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent3.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+        blogContent4.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+    
+    }
+
+    function updateBlog3Content() {
+       blogDocumentTitle.textContent = "The Role of National Parks";
+       blogBannerTitle.textContent = "The Role of National Parks";
+       blogBannerImage.src = '/media/giraffe-2.jpg';
+       blogImage1.src = '/media/blogcardimg3.jpg';
+       blogImage2.src = '/media/blogcardimg3.jpg';
+       blogAdmin.textContent = 'admin';
+       blogDate.textContent = '23 July, 2024',
+       blogComments.textContent = '0',
+       blogContent1.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+       blogContent2.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+       blogContent3.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+       blogContent4.textContent = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse per curabitur aptent laoreet fringilla lacus euismod placerat? Proin odio convallis taciti placerat posuere orci ornare quisque. Quis fames sit; quis sem eleifend molestie. Finibus duis justo felis cras ex montes. Bibendum praesent rutrum mollis dignissim justo tempus. Blandit maximus hendrerit per ultrices nec ac elementum. Pretium maecenas arcu hendrerit nascetur tortor porttitor parturient nascetur.';
+    
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const preLoader=document.querySelector("#preloader");
-window.addEventListener("load",()=>{
-    preLoader.style.display="none";
+const preLoader = document.querySelector("#preloader");
+window.addEventListener("load", () => {
+    preLoader.style.display = "none";
+})
+
+const projCards = document.querySelector(".proj-cards").children;
+Array.from(projCards).forEach((projCard) => {
+    const projCardId = projCard.getAttribute("id");
+    const projCardImg = projCard.querySelector("a");
+    projCardImg.href = `/pages/project-subpage.html?project=${projCardId}`;
+})
+
+const blogCards = document.querySelector(".blog-cards").children;
+Array.from(blogCards).forEach((blogCard) => {
+    const blogCardId = blogCard.getAttribute("id");
+    const blogCardButton = blogCard.querySelector("a");
+    blogCardButton.href = `/pages/blog-subpage.html?blog=${blogCardId}`;
 })
 
 const progressBars = document.querySelectorAll(".donation-progress");
@@ -8,37 +22,51 @@ const progressBars = document.querySelectorAll(".donation-progress");
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         const progressBarBox = entry.target.nextElementSibling;
-        const targetPercent=parseInt(entry.target.getAttribute("data-width"),10);
+        const targetPercent = parseInt(entry.target.getAttribute("data-width"), 10);
         let animated = false;
         if (entry.isIntersecting && !animated) {
             let initialPercent = 0;
             let main = setInterval(frame, 50);
             function frame() {
-                    if (initialPercent >= targetPercent) {
-                        clearInterval(main); 
+                if (initialPercent >= targetPercent) {
+                    clearInterval(main);
+                } else {
+                    initialPercent++;
+                    entry.target.style.width = initialPercent + "%";
+                    if ((initialPercent >= 92) && (initialPercent <= 100)) {
+                        progressBarBox.style.left = "93%";
                     } else {
-                        initialPercent++;
-                        entry.target.style.width = initialPercent + "%";
-                        if ((initialPercent >= 92) && (initialPercent <= 100)) {
-                            progressBarBox.style.left = "93%"; 
-                        } else {
-                            progressBarBox.style.left = `${initialPercent - 5 }%`;
-                        }
-                        progressBarBox.textContent = `${initialPercent}%`;
+                        progressBarBox.style.left = `${initialPercent - 5}%`;
                     }
+                    progressBarBox.textContent = `${initialPercent}%`;
+                }
             }
             animated = true;
             observer.unobserve(entry.target);
         }
-    })}, 
+    })
+},
     {
-    threshold: 0.5
+        threshold: 0.5
     }
 );
 
 progressBars.forEach((progressBar) => {
     observer.observe(progressBar);
-}); 
+});
+
+const observer2 = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("showY");
+        } else {
+            entry.target.classList.remove("showY");
+        }
+        observer.unobserve(entry.target);
+    })
+}, {
+    threshold: 0.1
+})
 
 // const homeBannerContent=document.querySelector(".home-banner").children;
 
@@ -48,19 +76,6 @@ progressBars.forEach((progressBar) => {
 //         content.classList.add("show");
 //     })
 // })
-
-const observer2=new IntersectionObserver((entries,observer)=>{
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("showY");
-        }else{
-            entry.target.classList.remove("showY");
-        }
-        observer.unobserve(entry.target);
-    }) 
-},{
-    threshold:0.1
-})
 
 // Array.from(homeBannerContent).forEach((content)=>{
 //     content.classList.add("hideY");
@@ -77,7 +92,7 @@ const observer2=new IntersectionObserver((entries,observer)=>{
 //             entry.target.classList.remove("showX");
 //         }
 //         observer.unobserve(entry.target);
-//     }) 
+//     })
 // },{
 //     threshold:0.1
 // })
